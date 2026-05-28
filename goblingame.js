cca88e3ec31bff7533155bdea89bcf074d1e6034
owkaufman6566 <owkaufman6566@student.pps.net>
2026-05-28 12:49:15 -0700

    update
    Display code when score reaches 10.

--- goblingame.js
+++ goblingame.js
@@ -2,15 +2,21 @@ var score = 0;
 const goblin = document.getElementById("goblin");
 const sound = new Audio('goblin-sound.mp3');
 function game() {
+    if(score >= 10){
+        document.getElementById("code").innerHTML = "09GKDL7.html"
+    }
     sound.load();
+  
     var randX = Math.floor(Math.random() * window.innerWidth / 2);
     var randY = Math.floor(Math.random() * window.innerHeight / 2);
     goblin.style.position = 'absolute';
     goblin.style.left = randX + 'px';
     goblin.style.top = randY + 'px';
+    
+   
     goblin.style.display = 'block';
     sound.play();
-    setTimeout(game, 1000);
+        setTimeout(game, 1000);
 }
 
 goblin.addEventListener('click', () => {
@@ -18,6 +24,6 @@ goblin.addEventListener('click', () => {
     document.getElementById("counter").innerHTML = "SCORE: " + score;
     goblin.style.display = 'none';
     
-    setTimeout(game, 1001);
+    setTimeout(game, 1000);
 });
 game();

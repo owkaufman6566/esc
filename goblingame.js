ce0c60a26b3c38ae1b0f1c5f50e932bc1dc4d75c
olpineo6700 <olpineo6700@student.pp.snet>
2026-05-28 13:50:47 -0700

    started puzzle 11 need to make image to finish puzzle

--- goblingame.js
+++ goblingame.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var score = 0;
 const goblin = document.getElementById("goblin");
 const sound = new Audio('goblin-sound.mp3');
@@ -28,34 +27,3 @@ goblin.addEventListener('click', () => {
     setTimeout(game, 1000);
 });
 game();
-=======
-var score = 0;
-const goblin = document.getElementById("goblin");
-const sound = new Audio('goblin-sound.mp3');
-function game() {
-    if(score >= 10){
-        document.getElementById("code").innerHTML = "09GKDL7.html"
-    }
-    sound.load();
-  
-    var randX = Math.floor(Math.random() * window.innerWidth / 2);
-    var randY = Math.floor(Math.random() * window.innerHeight / 2);
-    goblin.style.position = 'absolute';
-    goblin.style.left = randX + 'px';
-    goblin.style.top = randY + 'px';
-    
-   
-    goblin.style.display = 'block';
-    sound.play();
-        setTimeout(game, 1000);
-}
-
-goblin.addEventListener('click', () => {
-    score++;
-    document.getElementById("counter").innerHTML = "SCORE: " + score;
-    goblin.style.display = 'none';
-    
-    setTimeout(game, 1000);
-});
-game();
->>>>>>> cca88e3ec31bff7533155bdea89bcf074d1e6034
---
+++ lvl11button.js
@@ -0,0 +1,6 @@
+document.getElementById("answer").innerHTML = "the function name is 'supurb()' "
+
+function supurb(click){
+
+    document.getElementById("puzzle").innerHTML = "12D25LP.html"
+}    

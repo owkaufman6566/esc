c3225137d9b19fc95c8648343f13f73d1fcbd405
owkaufman6566 <owkaufman6566@student.pps.net>
2026-06-05 09:08:53 -0700

    update

---
+++ death.js
@@ -0,0 +1,12 @@
+function thankYou() {
+ alert('Curiosity is a blessing and a curse. You have found the end of this path, but there are many more to explore. Do not be afraid to venture into the unknown.');
+  setInterval(curiosityIsACurse, 100);
+}
+
+function curiosityIsACurse() {
+   
+    window.open("", "Finale", "width=400,height=400");
+
+    setInterval(curiosityIsACurse, 1000);
+    
+}
